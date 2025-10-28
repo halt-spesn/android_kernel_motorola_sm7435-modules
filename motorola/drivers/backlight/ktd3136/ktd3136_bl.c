56e15a23ed98b791f8000891b67585315cc51d2a
Nikita Savchenko <NikitaSavc@yandex.ru>
2025-10-28 21:01:50 +0000

    motorola: ktd3136: Fix set but unused error
    Change-Id: I54aa456addb6d77228d47dee4b4678a149c8adc5

--- motorola/drivers/backlight/ktd3136/ktd3136_bl.c
+++ motorola/drivers/backlight/ktd3136/ktd3136_bl.c
@@ -729,7 +729,6 @@ static int ktd3136_probe(struct i2c_client *client,
 {
 	struct ktd3136_data *drvdata;
 #ifdef KERNEL_ABOVE_4_14
-	struct backlight_device *bl_dev;
 	struct backlight_properties props;
 #endif
 	int err = 0;
@@ -793,7 +792,7 @@ static int ktd3136_probe(struct i2c_client *client,
 	props.type = BACKLIGHT_PLATFORM;
 	props.brightness = MAX_BRIGHTNESS;
 	props.max_brightness = MAX_BRIGHTNESS;
-	bl_dev = backlight_device_register(KTD3136_NAME, &client->dev,
+	backlight_device_register(KTD3136_NAME, &client->dev,
 					drvdata, &ktd3136_bl_ops, &props);
 	if (bl_dev ==NULL) {
 		pr_err("%s : bl_dev == NULL\n", __func__);
