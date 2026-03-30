f0c1b85483b4f9ab97ed4d848feb9bc75614ac1d
Nikita Savchenko <NikitaSavc@yandex.ru>
2026-03-30 09:41:06 +0000

    drivers: input: focaltech_0flash_v2_mmi: Mark variable as __maybe_unused
    Change-Id: I155a06f9351bb4a047883d4e8cf3b07aacd4ec7d

--- motorola/drivers/input/touchscreen/focaltech_0flash_v2_mmi/focaltech_mmi_class.c
+++ motorola/drivers/input/touchscreen/focaltech_0flash_v2_mmi/focaltech_mmi_class.c
@@ -284,7 +284,7 @@ static int fts_mmi_charger_mode(struct device *dev, int mode)
 static int fts_mmi_panel_state(struct device *dev,
 	enum ts_mmi_pm_mode from, enum ts_mmi_pm_mode to)
 {
-	struct fts_ts_data *ts_data = fts_data;
+	struct fts_ts_data *ts_data __maybe_unused = fts_data;
 
 #if defined(CONFIG_FTS_DOUBLE_TAP_CONTROL)
 	unsigned char gesture_type = 0;
