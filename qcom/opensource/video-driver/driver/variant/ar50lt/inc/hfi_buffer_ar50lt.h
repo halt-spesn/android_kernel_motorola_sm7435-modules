af44a4290e9890322b7474159afec2667de6f489
halt-spesn <ffoxy2870@gmail.com>
2026-06-25 17:44:07 +0000

    video-driver: Define MIN and MAX macros in hfi_buffer

--- qcom/opensource/video-driver/driver/variant/ar50lt/inc/hfi_buffer_ar50lt.h
+++ qcom/opensource/video-driver/driver/variant/ar50lt/inc/hfi_buffer_ar50lt.h
@@ -9,6 +9,12 @@
 #include <linux/types.h>
 #include "hfi_property.h"
 
+#ifndef MAX
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#endif
+#ifndef MIN
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+#endif
 typedef u8 HFI_U8;
 typedef s8 HFI_S8;
 typedef u16 HFI_U16;
