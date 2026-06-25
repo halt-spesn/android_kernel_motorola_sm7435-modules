7446fb9f161ed5a00987bd8b918049d1c0d9d674
halt-spesn <ffoxy2870@gmail.com>
2026-06-25 18:38:40 +0000

    fixup qcacld

--- qcom/opensource/wlan/platform/cnss2/main.c
+++ qcom/opensource/wlan/platform/cnss2/main.c
@@ -5925,6 +5925,16 @@ static void __exit cnss_exit(void)
 	cnss_debug_deinit();
 }
 
+void cnss_lock_pm_sem(struct device *dev)
+{
+}
+EXPORT_SYMBOL(cnss_lock_pm_sem);
+
+void cnss_release_pm_sem(struct device *dev)
+{
+}
+EXPORT_SYMBOL(cnss_release_pm_sem);
+
 module_init(cnss_initialize);
 module_exit(cnss_exit);
 
